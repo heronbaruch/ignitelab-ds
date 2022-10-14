cc19676c2fcaaa8aaba0c0b6be7a66481de635ea
Heron Baruch <33159457+heronbaruch@users.noreply.github.com>
2022-10-13 21:49:24 -0300

    text input terminado

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,5 +1,5 @@
-import { clsx } from 'clsx';
 import { InputHTMLAttributes, ReactNode } from 'react';
+import { Slot } from '@radix-ui/react-slot';
 
 
 export interface TextInputRootProps{
@@ -14,7 +14,17 @@ function TextInputRoot(props: TextInputRootProps) {
   )
 }
 
-// function TextInputIcon() {}
+export interface TextInputIconProps {
+  children: ReactNode;
+}
+
+function TextInputIcon(props: TextInputIconProps) {
+  return (
+    <Slot className='w-6 h-6 text-gray-400'>
+      {props.children}
+  </Slot>
+  )
+}
 
 export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement>{}
 
@@ -30,4 +40,5 @@ function TextInputInput(props: TextInputInputProps){
 export const TextInput = {
   Root: TextInputRoot,
   Input: TextInputInput,
+  Icon: TextInputIcon,
 }
